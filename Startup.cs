c35d60e3c3676de347064b22daa5a948a30bd99e
Ktheliwala <krishna.theliwala@gyrus.com>
2021-07-26 09:33:58 +0530

    Inital commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleGithubDemo.Startup))]
+namespace SampleGithubDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
